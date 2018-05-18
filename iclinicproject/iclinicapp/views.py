108800a0f3e0c6f4d7c9baad57436b78dcadae19
bussola <vbussola@yahoo.com>
2018-05-18 19:03:50 -0300

    swagger teste

--- iclinicproject/iclinicapp/views.py
+++ iclinicproject/iclinicapp/views.py
@@ -31,7 +31,6 @@ class AgendaViewSet(viewsets.ModelViewSet):
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
 
-    Additionally we also provide an extra `highlight` action.
     """
     queryset = Agenda.objects.all()
     serializer_class = AgendaSerializer
