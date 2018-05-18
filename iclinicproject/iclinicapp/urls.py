14964999588d62be0bb06c0504caaafd6c312713
Vinicius Bussola <vbussola@yahoo.com>
2018-05-18 11:25:14 -0300

    change urls

--- iclinicproject/iclinicapp/urls.py
+++ iclinicproject/iclinicapp/urls.py
@@ -1,14 +1,7 @@
-from django.conf.urls import url, include
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
-app_name = 'iclinicapp'
+from django.conf.urls import url
+from snippets import views
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
--- iclinicproject/iclinicapp/views.py
+++ iclinicproject/iclinicapp/views.py
@@ -1,25 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from iclinicapp.models import Snippet
+from iclinicapp.serializers import SnippetSerializer
 
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-#from iclinicproject.iclinicapp.serializers import UserSerializer, GroupSerializer
-from . import serializers
-
-
-class UserViewSet(viewsets.ModelViewSet):
+@csrf_exempt
+def snippet_list(request):
     """
-    API endpoint that allows users to be viewed or edited.
+    List all code snippets, or create a new snippet.
     """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = serializers.UserSerializer
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+@csrf_exempt
+def snippet_detail(request, pk):
     """
-    API endpoint that allows groups to be viewed or edited.
+    Retrieve, update or delete a code snippet.
     """
-    queryset = Group.objects.all()
-    serializer_class = serializers.GroupSerializer
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
 
-def pagina_inicial(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
