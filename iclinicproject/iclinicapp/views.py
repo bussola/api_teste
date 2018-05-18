e96b65d55b8782484982f4a5f8d66669863e06cb
Vinicius Bussola <vbussola@yahoo.com>
2018-05-18 10:06:16 -0300

    serializer_ajuste

--- iclinicproject/iclinicapp/views.py
+++ iclinicproject/iclinicapp/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from iclinicproject.iclinicapp.serializers import UserSerializer, GroupSerializer
+#from iclinicproject.iclinicapp.serializers import UserSerializer, GroupSerializer
+from . import serializers
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,7 +11,7 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -18,7 +19,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    serializer_class = serializers.GroupSerializer
 
 def pagina_inicial(request):
     return render(request, 'index.html')
