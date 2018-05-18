1e1ad137a17530264f04648e893c79523400572a
Vinicius Bussola <vbussola@yahoo.com>
2018-05-18 11:34:53 -0300

    change postgres

--- iclinicproject/iclinicproject/settings.py
+++ iclinicproject/iclinicproject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'iclinicproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_iclinic',
         'USER': 'iclinic',
         'PASSWORD': 'senha',
