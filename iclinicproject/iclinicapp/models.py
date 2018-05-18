0899e5cb7d181f4f8f81009997a819ba48966460
Vinicius Bussola <vbussola@yahoo.com>
2018-05-18 16:15:53 -0300

    add dropdown

--- iclinicproject/iclinicapp/models.py
+++ iclinicproject/iclinicapp/models.py
@@ -6,10 +6,15 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+# LEXERS = [item for item in get_all_lexers() if item[1]]
+# LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+# STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
+PROC_CHOICES = (
+    ('consulta','CONSULTA'),
+    ('retorno', 'RETORNO'),
+    ('horario bloqueado','HORARIO BLOQUEADO'),
+)
 
 class Snippet(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
@@ -18,7 +23,8 @@ class Snippet(models.Model):
 	hor_inicio = models.TimeField(blank=True)
 	hor_final = models.TimeField(blank=True)
 	paciente = models.CharField(max_length=100, blank=True, default='')
-	procedimento = models.CharField(max_length=100, blank=True, default='')
+	#procedimento = models.CharField(max_length=100, blank=True, default='')
+	procedimento = models.CharField(max_length=6, choices=PROC_CHOICES, default='consulta')
 
 	# title = models.CharField(max_length=100, blank=True, default='')
 	# code = models.TextField()
