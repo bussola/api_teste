2a6dd8ab4e7290d183fb2c14c57ba16cc13d3b43
bussola <vbussola@yahoo.com>
2018-05-19 10:55:17 -0300

    ajuste datetime

--- iclinicproject/iclinicapp/models.py
+++ iclinicproject/iclinicapp/models.py
@@ -19,7 +19,7 @@ PROC_CHOICES = (
 class Agenda(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 
-	data = models.DateField(blank=True, format="%d-%m-%Y", input_formats=['%d-%m-%Y', 'iso-8601'])
+	data = models.DateField(blank=True)
 	hora_inicio = models.TimeField(blank=True)
 	hora_final = models.TimeField(blank=True)
 	paciente = models.CharField(max_length=100, blank=True, default='')
