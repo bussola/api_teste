f12f6f05efcf26fa57b0244b8688d466faa33bb6
bussola <vbussola@yahoo.com>
2018-05-19 12:24:58 -0300

    ajuste testes

--- iclinicproject/iclinicapp/tests.py
+++ iclinicproject/iclinicapp/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from iclinicapp.models import Agenda
 
-# Create your tests here.
+import unittest
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+    def setUp(self):
+        self.agenda = Agenda.objects.all()
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        assert a == 1
