c44329dc22bd5809a2fca59f1d98394b52caf75e
bussola <vbussola@yahoo.com>
2018-05-20 14:22:40 -0300

    testes

--- iclinicproject/iclinicapp/views.py
+++ iclinicproject/iclinicapp/views.py
@@ -18,9 +18,6 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 
 
-from rest_framework import mixins
-#from rest_framework_bulk.mixins import BulkUpdateModelMixin
-
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
@@ -29,17 +26,39 @@ def api_root(request, format=None):
     })
 
 
-
-#class AgendaViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, BulkUpdateModelMixin, generics.GenericAPIView):
 class AgendaViewSet(viewsets.ModelViewSet):
     """
-    ---
-    GET:
-        consumes:
-            - application/vnd.api+json
-        produces:
-            - application/vnd.api+json
+    retrieve:
+        Return a meeting instance.
+
+    list:
+        Return all meeting, ordered by most recently added.
+
+    create:
+        Creates a new Meeting
+
+    delete:
+        Remove an existing meeting.
+
+    partial_update:
+        Update one or more fields on an existing meeting.
+
+    update:
+        Update a meeting.
     """
+
+    queryset = Agenda.objects.all()
+    serializer_class = AgendaSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    # @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    # def highlight(self, request, *args, **kwargs):
+    #     agenda = self.get_object()
+    #     return Response(agenda.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -47,69 +66,16 @@ class AgendaViewSet(viewsets.ModelViewSet):
     def put(self, request, *args, **kwargs):
         return self.bulk_update(request, *args, **kwargs)
 
-    def patch(self, request, *args, **kwargs):
-        return self.partial_bulk_update(request, *args, **kwargs)
+    # def list(self, request):
+    #     queryset = User.objects.all()
+    #     serializer = UserSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def get_queryset(self):
-        return Agenda.objects.all()
-
-    def get_serializer_class(self):
-        return AgendaSerializer
-
-
-
-
-
-
-
-
-# class AgendaViewSet(viewsets.ModelViewSet):
-#     """
-#     retrieve:
-#         Return a meeting instance.
-
-#     list:
-#         Return all meeting, ordered by most recently added.
-
-#     create:
-#         Creates a new Meeting
-
-#     delete:
-#         Remove an existing meeting.
-
-#     partial_update:
-#         Update one or more fields on an existing meeting.
-
-#     update:
-#         Update a meeting.
-#     """
-
-#     queryset = Agenda.objects.all()
-#     serializer_class = AgendaSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-#                           IsOwnerOrReadOnly,)
-
-#     # @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-#     # def highlight(self, request, *args, **kwargs):
-#     #     agenda = self.get_object()
-#     #     return Response(agenda.highlighted)
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
-#     # def list(self, request):
-#     #     queryset = User.objects.all()
-#     #     serializer = UserSerializer(queryset, many=True)
-#     #     return Response(serializer.data)
-
-#     # def retrieve(self, request, pk=None):
-#     #     queryset = User.objects.all()
-#     #     user = get_object_or_404(queryset, pk=pk)
-#     #     serializer = UserSerializer(user)
-#     #     return Response(serializer.data)
+    # def retrieve(self, request, pk=None):
+    #     queryset = User.objects.all()
+    #     user = get_object_or_404(queryset, pk=pk)
+    #     serializer = UserSerializer(user)
+    #     return Response(serializer.data)
         
 
 
