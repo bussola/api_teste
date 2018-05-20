be1f4456b65399d7aa7fffe4f7d66329aeb844a9
bussola <vbussola@yahoo.com>
2018-05-19 21:48:13 -0300

    ajuste tests

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -2,14 +2,15 @@ from django.test import TestCase
 from iclinicapp.models import Agenda
 
 class AnimalTestCase(TestCase):
-    # def setUp(self):
-    #     Agenda.objects.create(data="05/09/2018", hora_inicio="10:10:AM")
-    #     Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
+	def setUp(self):
+		Agenda.objects.create(data="05/09/2018", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta")
+		#Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
 
-    def test_1(self):
-        a = 1
-        assert a == 1
+	def test_1(self):
+		instance = Agenda.objects.values('data')[0]
+		description = instance['description']
+		self.assertEqual(description, '05/09/2018')
 
-    def test_2(self):
-        a = 1
-        assert a == 2
+	def test_2(self):
+		a = 1
+		assert a == 2
