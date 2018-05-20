c34cf7beed50f97b11501749d2dd3de9e5ea9c99
bussola <vbussola@yahoo.com>
2018-05-19 21:49:44 -0300

    ajuste tests

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -3,13 +3,13 @@ from iclinicapp.models import Agenda
 
 class AnimalTestCase(TestCase):
 	def setUp(self):
-		Agenda.objects.create(data="05/09/2018", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta")
+		Agenda.objects.create(data="2018/05/19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta")
 		#Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
 
 	def test_1(self):
 		instance = Agenda.objects.values('data')[0]
 		description = instance['description']
-		self.assertEqual(description, '05/09/2018')
+		self.assertEqual(description, '2018/05/19')
 
 	def test_2(self):
 		a = 1
