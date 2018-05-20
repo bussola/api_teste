16141bc419628ce6d8bb45c9771a29c3a892050a
bussola <vbussola@yahoo.com>
2018-05-19 21:52:24 -0300

    ajuste tests

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -3,7 +3,7 @@ from iclinicapp.models import Agenda
 
 class AnimalTestCase(TestCase):
 	def setUp(self):
-		Agenda.objects.create(data="2018-05-19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta")
+		Agenda.objects.create(data="2018-05-19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta", owner="iclinic", highlighted="shero")
 		#Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
 
 	def test_1(self):
