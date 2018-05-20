478c77c325da3149e5ec3be570421710f9788c14
bussola <vbussola@yahoo.com>
2018-05-19 22:46:35 -0300

    testes

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 from iclinicapp.models import Agenda
+from rest_framework import serializers
+
 
 class AnimalTestCase(TestCase):
 	def setUp(self):
-		Agenda.objects.create(data="2018-05-19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta", owner="iclinic", highlighted="shero")
+		owner = serializers.ReadOnlyField(source='owner.username')
+		Agenda.objects.create(data="2018-05-19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta", owner=owner, highlighted="shero")
 		#Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
 
 	def test_1(self):
