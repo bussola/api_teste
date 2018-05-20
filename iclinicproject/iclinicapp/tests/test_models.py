eac1878e8670d7e5b288e6759a88d09f6c33c32b
bussola <vbussola@yahoo.com>
2018-05-20 11:30:31 -0300

    testes

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -5,18 +5,14 @@ import datetime
 
 
 
-class AnimalTestCase(TestCase):
+class IclinicModelsTestCase(TestCase):
 	def setUp(self):
 		u = User(username="Jose")
 		u.save()
 		user = u
-		# User.objects.create(username="Jose")
-		# user = User.objects.values('username')[0]
-		#user = User.objects.get(id=user_id)
 		Agenda.objects.create(data="2018-05-19", hora_inicio="10:10:AM", hora_final="10:10:AM", paciente="Jose", procedimento="Consulta", owner=user, highlighted="shero")
-		#Agenda.objects.create(data="05/09/2017", hora_inicio="10:10:AM")
 
-	def test_1(self):
+	def test_date(self):
 		instance = Agenda.objects.values('data')[0]
 		description = instance['data']
 		self.assertEqual(description, datetime.date(2018, 5, 19))
