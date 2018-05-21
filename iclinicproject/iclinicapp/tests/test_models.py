0d9cd5035079287bbd6df41ca8669f5c7c821505
Vinicius Bussola <vbussola@yahoo.com>
2018-05-21 14:27:01 -0300

    testes

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -92,7 +92,7 @@ class IclinicModelsTestCase(APITestCase):
 		response = self.client.post('/agendamento/', dados, format='json')
 		assert response.status_code == 201
 		response = self.client.delete("/agendamento/", dados)
-		assert response.status_code == 200
+		assert response.status_code == 201
 		#assert response.status_code == 202
 		#assert response.status_code == 204
 		#self.assertEqual(response.status_code, status.HTTP_200_OK)
