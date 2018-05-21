7b2431b02d63ed71a5445af0cc6ab509237103b4
bussola <vbussola@yahoo.com>
2018-05-20 23:45:36 -0300

    .

--- iclinicproject/iclinicapp/serializers.py
+++ iclinicproject/iclinicapp/serializers.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 import datetime
 
 class AgendaSerializer(serializers.HyperlinkedModelSerializer):
-    #owner = serializers.ReadOnlyField(source='owner.username')
-    owner =  self.context['request'].user
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Agenda
