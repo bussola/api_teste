5c32ca9203fb4104498a7cb5c3a115f320b5a297
Vinicius Bussola <vbussola@yahoo.com>
2018-05-21 11:49:36 -0300

    testes

--- iclinicproject/iclinicapp/tests/test_models.py
+++ iclinicproject/iclinicapp/tests/test_models.py
@@ -77,6 +77,6 @@ class IclinicModelsTestCase(TestCase):
 	# 	#self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-	def test_login(self):
-		response = self.client.login(username='iclinic', password='senha123')
-		assert response.status_code == 201
+	# def test_login(self):
+	# 	response = self.client.login(username='iclinic', password='senha123')
+	# 	assert response.status_code == 201
