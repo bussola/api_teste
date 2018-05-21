f3c19fa2fc204b4bda01cc651693a1641140d1f1
bussola <vbussola@yahoo.com>
2018-05-20 23:21:52 -0300

    .

--- iclinicproject/iclinicproject/settings.py
+++ iclinicproject/iclinicproject/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'iclinicproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'db_iclinic',
-    #     'USER': 'iclinic',
-    #     'PASSWORD': 'senha',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_iclinic',
+        'USER': 'iclinic',
+        'PASSWORD': 'senha',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
